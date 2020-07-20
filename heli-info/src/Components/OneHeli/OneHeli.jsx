b5d2e508b0f4185b205490ab259c453e1f60192d
jordancheng604 <jordancheng604@gmail.com>
2020-07-20 15:14:39 -0700

    Chinook info: 99%.
    Chinook info is there, would be great if unit cost can be found.

--- heli-info/src/Components/OneHeli/OneHeli.jsx
+++ heli-info/src/Components/OneHeli/OneHeli.jsx
@@ -6,7 +6,6 @@ function OneHeli({singleAircraft}) {
     console.log(singleAircraft)
     return (
         <div className='OneHeli'>
-            <h5>One Heli is for displaying a single helicopter after a user clicked on one.</h5>
             <img src={singleAircraft.image}/>
             <h5>Manufacture: {singleAircraft.manufacturer_code}</h5>
             <div>Model number: {singleAircraft.model_no}</div>
