c2d41dde6e9adaf827434f6c4aa1bc462e2e1016
jordancheng604 <jordancheng604@gmail.com>
2020-07-19 21:51:13 -0700

    About and Contact pages.
    About: has content. go over again to edit, proof read.
    Contact: contact form needs WORK.

--- heli-info/src/Pages/Contact.jsx
+++ heli-info/src/Pages/Contact.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {Component} from 'react'
 import NavBar from '../Components/NavBar/NavBar';
+import { Link, Redirect, withRouter } from "react-router-dom";
 
-export default function Contact() {
+export default class Contact extends Component {
+    constructor(){
+        super();
+        this.state = { 
+            contactPage: '', 
+            name: 'Name', 
+            email: 'email@example.com'
+        };
+        this.handleChange=this.handleChange.bind(this);
+        // this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    render(){
     return (
         <div>
             <NavBar/>
-            <h1>Contact PAGE.</h1>
+            <form className="contact__form">
+
+                <input className="contact__nameInput" placeholder="Name"></input>
+                <input className="contact__emailInput" placeholder="E-mail"></input>
+                <textarea className="contact__messageInput" onChange={this.handleChange} placeholder="Message"></textarea>
+                <button className="contact__submitButton" onClick={this.handleSubmit}>SUBMIT</button>
+            </form>
+            {/* <h1>Contact PAGE.</h1>
             <p>
                 Contact us at:
                 heli.info2020@gmail.com
@@ -13,16 +34,17 @@ export default function Contact() {
             </p>
             <p>
                 EMAIL subbmission contact form.
-            </p>
+            </p> */}
 
-            {/* <form className="contact__form" onSubmit={""}>
+            {/* <form className="contact__form" >
                 <input name="emailInput" type="email" className="contact__emailInput">EMAIL</input>
                 <input name="nameInput" type="text" className="contact__nameInput">NAME</input>
-                <textarea type="text" className="contact__messageInput">Message</textarea>
-                <button>SUBMIT</button>
+                <textarea name="messageInput" type="text" className="contact__messageInput">Message</textarea>
+                <button className="contact__button">SUBMIT</button>
             </form> */}
 
             
         </div>
     )
+}
 }
