92c6b82d4a71c924d70dc922847ba1d3a4b17808
jordancheng604 <jordancheng604@gmail.com>
2020-07-21 21:39:59 -0700

    WIP - scroll trigger
    SCSS is not connecting/talking with StoryMode.

--- heli-info/src/Components/StoryMode/StoryMode.jsx
+++ heli-info/src/Components/StoryMode/StoryMode.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
 import NavBar from '../NavBar/NavBar';
+import './StoryMode.scss';
 
-export default function StoryMode() {
+function StoryMode() {
     return (
         <div>
             <NavBar/>
             <h5>StoryMode page</h5>
 {/* We'll base it off the Apache for now since there's more modeling data for it */}
-            <h3>Helicopter Model No and Name: AH-64 Apache</h3>
-            <h3>Manufacturer: Boeing</h3>
-            <p>Possible a slogan or nickname: The flying Takn.</p>
+            <h3 className="secion__left">Helicopter Model No and Name: AH-64 Apache</h3>
+            <h3 className="secion__left">Manufacturer: Boeing</h3>
+            <p className="secion__left">Possible a slogan or nickname: The flying Tank.</p>
 
 
             <div className="section__right">
@@ -17,13 +18,31 @@ export default function StoryMode() {
             </div>
 
             <div className="background__container">
-            
+            <div className="parallaxGround">For ground layer</div>
 
+            <div className="section__right">
+                <h3>More Apache info-RightSide</h3>
+                <p>And some more</p>
+            </div>
+
+            <div className="secion__left">
+                <h3>LeftSide</h3>
+                <p>left-side</p>
+            </div>
+
+            <div className="section__right">
+                <h3>stuf stuff</h3>
+                <p>mor stuff</p>
+            </div>
+
+            <div className="parallaxClouds">Clouds layer</div>
 
 
             </div>
 
+            <div>BluePrint section starts here.</div>
 
         </div>
     )
 }
+export default StoryMode;
