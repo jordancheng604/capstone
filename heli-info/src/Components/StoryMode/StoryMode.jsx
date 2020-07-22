cdbde822367638176a26157b1a16d5b62a873333
jordancheng604 <jordancheng604@gmail.com>
2020-07-22 13:39:20 -0700

    Save Point - trying to render a 3d cube.

--- heli-info/src/Components/StoryMode/StoryMode.jsx
+++ heli-info/src/Components/StoryMode/StoryMode.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+import * as THREE from "three";
+
 import NavBar from '../NavBar/NavBar';
 import './StoryMode.scss';
 
+
 import {OBJModel} from 'react-3d-viewer';
+import { Geometry } from 'three';
 
+const scene = new THREE.Scene();
 
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,
+    0.1,
+    1000
+    );
+
+const renderer = new THREE.WebGLRenderer(); 
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry( 2, 2, 2);
+const material = new THREE.MeshBasicMaterial( {color: 0x0000ff} );
+const cube = new THREE.Mesh( geometry, material );
+scene.add(cube);
 
 class StoryMode extends React.Component{
     constructor(props){
