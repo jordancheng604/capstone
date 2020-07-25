b5310528ce783cade4893c8404a40ec10b9ca921
jordancheng604 <jordancheng604@gmail.com>
2020-07-25 10:14:12 -0700

    Populating CDN DND helicopter data
    -images can be taken from DND site, they are rendering.
    -href's being used to reference DND RCAF resources.

--- heli-info/src/Components/OneHeli/OneHeli.jsx
+++ heli-info/src/Components/OneHeli/OneHeli.jsx
@@ -6,11 +6,11 @@ function OneHeli({singleAircraft}) {
     console.log(singleAircraft)
     return (
         <div className='OneHeli'>
-            <img src={singleAircraft.image}/>
-            <h5>Manufacture: {singleAircraft.manufacturer_code}</h5>
-            <div>Model number: {singleAircraft.model_no}</div>
-            <div>Model name: {singleAircraft.model_name}</div>
-            <p>{singleAircraft.overview}</p>
+            <img className='OneHeli__image' src={singleAircraft.image}/>
+            <h5 className='OneHeli__make'>Manufacture: {singleAircraft.manufacturer_code}</h5>
+            <div className='OneHeli__modelNumber'>Model number: {singleAircraft.model_no}</div>
+            <div className='OneHeli__modelName'>Model name: {singleAircraft.model_name}</div>
+            <p className='OneHeli__overview'>{singleAircraft.overview}</p>
         </div>
 
     )
