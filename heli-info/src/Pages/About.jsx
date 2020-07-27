16ce12de6300b55050762b0ef801b4a398251855
jordancheng604 <jordancheng604@gmail.com>
2020-07-26 17:32:23 -0700

    Contact Page: mobile and tablet versions acceptbale.

--- heli-info/src/Pages/About.jsx
+++ heli-info/src/Pages/About.jsx
@@ -5,26 +5,28 @@ import '../styles/styles.scss';
 
 export default function About() {
     return (
-        <div>
+        <div className='about'>
             <NavBar/>
             <h1>About Us</h1>
+            <div className='about__us'>
             <p>
-                The original intent of this is to help share and spread information about Aviation Helicopters and saying Thank-You to those that serve and have served.
+                The original intent of this is to help share and spread information about Aviation Helicopters and saying Thank-You to those that serve and have served.</p>
 
-                Thank you for the sacrifices that you and your family have made. 
+                <p> Thank you for the sacrifices that you and your family have made. </p>
 
-                We believe that there is not enough appreication for those that serve and have served; and therefore, encourage people to look into voltuneering or donations. If you're thinking about helping out somewhere, please consider our veterans.
+                <p>We believe that there is not enough appreciation for those that serve and have served; and therefore, encourage people to... 
+                    ...look into volunteering or donations. If you're thinking about helping out somewhere, please consider our veterans.</p>
 
-                This project started with Canadian Military Helicopters, and will continue to slowly grow and add more Military Aircraft and Equipment to the website.
+                <p>This project started with Canadian Military Helicopters, and we would like to slowly grow and add more Military Aircraft and Equipment to the website. It is dependant on a lot of factors so please bear with us if adding new content is slow.</p>
 
-                We are a small team, so please me patient with us if you do not see the aircraft you are looking for.
+                <p>Also, we are a small team, so please be patient with us if you do not see the aircraft you are looking for.</p>
 
-                Our goal is to eventually have all NATO military equipment listed on our website.
+                <p>Our goal is to eventually have all NATO military equipment listed on our website.</p>
 
-                There have been unforseen difficulties in collecting appropriate information and content to not break any copyright laws.
+                <p>There have been unforeseen difficulties in collecting appropriate information and content to not break any copyright laws.</p>
 
-                If you would like to see your favorite Aircraft listed on our website. Feel free to use the Contact page and let us know. And if you have a favorite picture, that you own, and would like have it on our website please kindly send it to us via email.
-            </p>
+                <p>If you would like to see your favorite Aircraft listed on our website. Feel free to use the Contact page and let us know. And if you have a favorite picture, that you own, and would like have it on our website please kindly send it to us via email.</p>
+            </div>
         </div>
     )
 }
--- heli-info/src/Pages/Contact.jsx
+++ heli-info/src/Pages/Contact.jsx
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
 import NavBar from '../Components/NavBar/NavBar';
 import { Link, Redirect, withRouter } from "react-router-dom";
-import '../styles/styles.scss';
+// import '../styles/styles.scss';
+import '../styles/ContactPage.scss';
 
 export default class Contact extends Component {
     constructor(){
@@ -19,13 +20,27 @@ export default class Contact extends Component {
     render(){
     return (
         <div>
-            <NavBar/>
+        <NavBar/>
+            <div className="contact">
             <h1>Contact Us</h1>
+            <p className="contact__usPage">Don't see the aircraft you're looking for, or have a picture that you've taken and want to share? Feel free to reach out and we'll see what we can do.</p>
+           <div className="contact__usPageImage">?... hello?...</div>
             <form className="contact__form">
-
+                <div className="contact__nameContainer">
+                <label>Name: </label>
                 <input className="contact__nameInput" placeholder="Name"></input>
+                </div>
+
+                <div className="contact__emailContainer">
+                <label >E-mail: </label>
                 <input className="contact__emailInput" placeholder="E-mail"></input>
+                </div>
+
+                <div className="contact__messageContainer">
+                <label className="contact__messageLabel">Message: </label>
                 <textarea className="contact__messageInput" onChange={this.handleChange} placeholder="Message"></textarea>
+                </div>
+
                 <button className="contact__submitButton" onClick={this.handleSubmit}>SUBMIT</button>
             </form>
             {/* <h1>Contact PAGE.</h1>
@@ -45,7 +60,7 @@ export default class Contact extends Component {
                 <button className="contact__button">SUBMIT</button>
             </form> */}
 
-            
+</div>
         </div>
     )
 }
